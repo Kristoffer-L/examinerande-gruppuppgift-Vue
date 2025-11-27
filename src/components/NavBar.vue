d15257029dda672c7879ee1dd7882a666beaefa3
Andreas <andreas.johansson@chasacademy.se>
2025-11-27 12:11:11 +0100

    created basket icon svg

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import BasketIcon from "../icons/BasketIcon.vue";
 </script>
 
 <template>
-  <nav class=" px-6 py-3 flex items-center">
+  <nav class="px-6 py-3 flex items-center">
     <img src="../assets/Logo.png" alt="Logo" class="mr-auto" />
-    <RouterLink to="/" class="hover:text-gray-300 font-semibold text-2xl mr-2">Home</RouterLink>
-    <svg src="../../public/Basket.svg" class="h-6 w-6 text-white mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-    </svg>
-</nav> 
+    <RouterLink to="/" class="hover:text-gray-300 font-semibold text-2xl mr-2"
+      >Home</RouterLink>
+          <RouterLink to="/basket" class="ml-2 mr-2"><BasketIcon /> </RouterLink>
+  </nav>
 </template>
