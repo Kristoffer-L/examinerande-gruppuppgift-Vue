5a84079c19b896a4576407702cbf4b7ad038285b
Andreas Johansson <andreas.pi.johansson@chasacademy.se>
2025-12-08 11:26:15 +0100

    added promo pictures. added start of experience component

---
+++ src/components/ExperienceComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  image: string;
+  title: string;
+  desc: string;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+</script>
+
+<template>
+  {{ props.image }}
+  {{ props.title }}
+  {{ props.desc }}
+  <button @click="$emit('click')">Read More</button>
+  <button @click="$emit('click')">Book</button>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-document.title = "Simple Page | My App";
+import ExperienceComponent from '../components/ExperienceComponent.vue';
+document.title = 'Simple Page | My App';
 </script>
 
 <template>
   <div class="min-h-screen p-8">
     <h1 class="text-2xl font-bold mb-4">Simple Page</h1>
     <p>This is a light scaffold for quick pages.</p>
+    <ExperienceComponent label="Submit" />
   </div>
 </template>
